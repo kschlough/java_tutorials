a8e13f0b6ce78f32b8b4eef9ab06db3e58b702d9
kschlough <61514488+kschlough@users.noreply.github.com>
2021-09-16 16:20:49 -0700

    practice java version of for i in range()

--- Loops.java
+++ Loops.java
@@ -5,15 +5,26 @@ public class Loops {
     public static void main(String[] args) {
     
     // for i in range(1,100)
-        for (int i = 0; i <= 100; i++) {
-            System.out.print(i);
+        System.out.print("for i in range(1,100): ");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
         }
 
     // for i in range(2,101,2)
+        System.out.print("for i in range(2,101,2): ");
+        for (int i = 2; i <= 101; i += 2) {
+            System.out.println(i);
+        }
 
     // for i in range(100,0,-1)
+        System.out.print("for i in range(100,0,-1): ");
+        for (int i = 100; i >= 0; i -= 1) {
+            System.out.println(i);
+        }
 
     // for x,y in zip(range(10),range(0,20,2))
+        System.out.print("for x,y in zip(range(10),range(0,20,2)): ");
+        // separate statements in the same clause with ,]
     }
 
 }
