ca9f7e9fbf0ea6dc2d1f553f9685c392c3ff4463
kschlough <61514488+kschlough@users.noreply.github.com>
2021-09-16 16:14:00 -0700

    practice rewriting python loops in java

---
+++ Loops.java
@@ -0,0 +1,19 @@
+// rewrite the python loops as java
+
+public class Loops {
+
+    public static void main(String[] args) {
+    
+    // for i in range(1,100)
+        for (int i = 0; i <= 100; i++) {
+            System.out.print(i);
+        }
+
+    // for i in range(2,101,2)
+
+    // for i in range(100,0,-1)
+
+    // for x,y in zip(range(10),range(0,20,2))
+    }
+
+}
